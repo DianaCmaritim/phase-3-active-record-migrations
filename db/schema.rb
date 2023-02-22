9c510d8b313714f558f7922aa75568b64296ee6a
Diana <maritimdiane96@gmail.com>
2023-02-22 08:26:26 +0300

    completed assignment

--- db/schema.rb
+++ db/schema.rb
@@ -15,7 +15,7 @@ ActiveRecord::Schema.define(version: 2023_02_21_124716) do
   create_table "artists", force: :cascade do |t|
     t.string "name"
     t.string "genre"
-    t.string "age"
+    t.integer "age"
     t.string "hometown"
     t.string "favorite_flower"
   end
